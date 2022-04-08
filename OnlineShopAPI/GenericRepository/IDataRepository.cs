450ac166d185dcdacbfae1112848ed85b17bb922
ManeMag <akkofsasha@gmail.com>
2022-04-08 20:07:43 +0300

    Added intendants in IDataRepository

--- OnlineShopAPI/GenericRepository/IDataRepository.cs
+++ OnlineShopAPI/GenericRepository/IDataRepository.cs
@@ -8,12 +8,19 @@ namespace GenericRepository
     public interface IDataRepository<T> where T : class
     {
         Task<T> GetAsync(int id);
+
         Task<IEnumerable<T>> GetAllAsync();
+
         Task AddAsync(T entity);
+
         Task AddRangeAsync(IEnumerable<T> entities);
+
         Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> expression);
+
         void Update(T entity);
+
         void Remove(T entity);
+
         void RemoveRange(IEnumerable<T> entities);
     }
 }
