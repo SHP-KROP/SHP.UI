8e00bd59a052ca979819557582872beb45784c0b
Ruslan Tretyakov <6968597@stud.nau.edu.ua>
2022-04-08 17:54:13 +0300

    Implemented repository interface & class

---
+++ OnlineShopAPI/GenericRepository/DataRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace GenericRepository
+{
+    class DataRepository<T> : IDataRepository<T> where T : class
+    {
+        private readonly DbContext _context;
+        public DataRepository(DbContext context)
+        {
+            _context = context;
+        }
+        public async Task<T> GetAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+        }
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _context.Set<T>().AddRangeAsync(entities);
+        }
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> expression)
+        {
+            var query = _context.Set<T>().Where(expression);
+
+            return await query.ToListAsync();
+        }
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+        public void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
+        }
+    }
+}
---
+++ OnlineShopAPI/GenericRepository/IDataRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace GenericRepository
+{
+    interface IDataRepository<T> where T : class
+    {
+        Task<T> GetAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> expression);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
