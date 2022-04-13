500f809214855ba1da680225f09fc3e01b766661
ManeMag <akkofsasha@gmail.com>
2022-04-13 13:29:44 +0300

    Added tests project

---
+++ OnlineShopAPI/IdentityServerTests/UserControllerTests.cs
@@ -0,0 +1,22 @@
+using IdentityServer.Controllers;
+using NUnit.Framework;
+
+namespace IdentityServerTests
+{
+    public class Tests
+    {
+        private UserController _userController;
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
