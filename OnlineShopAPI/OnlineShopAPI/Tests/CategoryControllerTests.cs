5ca3f76135168a4e7fb1158140d59704736fd044
ManeMag <akkofsasha@gmail.com>
2022-04-13 16:34:41 +0300

    Tests classes and dependencies added

---
+++ OnlineShopAPI/OnlineShopAPI/Tests/CategoryControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using OnlineShopAPI.Controllers;
+using Xunit;
+
+namespace OnlineShopAPI.Tests
+{
+    public class CategoryControllerTests
+    {
+        private ProductController _productController;
+
+        public CategoryControllerTests()
+        {
+
+        }
+
+        [Fact]
+        public void Test()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
---
+++ OnlineShopAPI/OnlineShopAPI/Tests/ProductControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using OnlineShopAPI.Controllers;
+using Xunit;
+
+namespace OnlineShopAPI.Tests
+{
+    public class ProductControllerTests
+    {
+        private ProductController _productController;
+
+        public ProductControllerTests()
+        {
+
+        }
+
+        [Fact]
+        public void Test()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
