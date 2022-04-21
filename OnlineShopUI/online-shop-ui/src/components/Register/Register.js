66afb5d04833837dfae356303f92085fe905d66b
ManeMag <akkofsasha@gmail.com>
2022-04-21 11:04:39 +0300

    Main functionality of registration works

--- OnlineShopUI/online-shop-ui/src/components/Register/Register.js
+++ OnlineShopUI/online-shop-ui/src/components/Register/Register.js
@@ -8,16 +8,44 @@ const BASE_URL = "https://localhost:44330/api/user/register/";
 const Register = () => {
   const [name, setUsername] = useState(() => "");
   const [pass, setPassword] = useState(() => "");
+  const [flag, setFlag] = useState(() => true);
 
-  function register() {
+  const [response, setResponse] = useState(() => {});
+
+  useEffect(() => {
+    if (!(name && pass)) {
+      return;
+    }
+    console.log("Calling api started");
     axios
       .post(BASE_URL, {
         userName: name,
         password: pass,
       })
-      .then((response) => response.data)
-      .then((data) => localStorage.setItem("RESPONSE", data));
-  }
+      .then((response) => {
+        setResponse(response.data);
+        proceedResponse(response.data);
+      })
+      .catch((error) => {
+        if (error.response.status === 400) {
+          alert("You cannot register such a user");
+        } else if (error.request) {
+          console.log(error.request);
+        } else if (error.message) {
+          console.log(error.message);
+        }
+      });
+  }, [flag]);
+
+  const proceedResponse = (response) => {
+    try {
+      alert(`Welcome ${response.userName}`);
+      localStorage.setItem("token", response.token);
+    } catch (ex) {
+      console.log(ex);
+      alert("Impossible to register such a user");
+    }
+  };
 
   return (
     <>
@@ -48,10 +76,10 @@ const Register = () => {
                 setPassword(e.target.value);
               }}
             />
-            <button className="btn" type="submit" onClick={register}>
-              Submit
-            </button>
           </form>
+          <button className="btn" type="submit" onClick={() => setFlag(!flag)}>
+            Submit
+          </button>
         </div>
       </div>
     </>
