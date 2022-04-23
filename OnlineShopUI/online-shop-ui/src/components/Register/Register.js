2f49e66f0f5b166cf7218662a307507ae34fe3cf
ManeMag <akkofsasha@gmail.com>
2022-04-23 20:33:48 +0300

    Added notifications for register

--- OnlineShopUI/online-shop-ui/src/components/Register/Register.js
+++ OnlineShopUI/online-shop-ui/src/components/Register/Register.js
@@ -1,56 +1,83 @@
-import { Link } from '@mui/material';
-import { React, useState, useEffect } from 'react';
-import { AuthService } from '../../_services/AuthService';
-import axios from 'axios';
-import './Register.scss';
-import RegisterBG from '../../img/banner-bg.png';
+import { Link, LinearProgress } from "@mui/material";
+import { React, useState, useEffect, useRef } from "react";
+import { toast } from "react-toastify";
+import { Navigate } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import "./Register.scss";
+import RegisterBG from "../../img/banner-bg.png";
 
-const BASE_URL = 'https://localhost:44330/api/user/register/';
+const BASE_URL = "https://localhost:44330/api/user/register/";
+
+toast.configure();
 
 const Register = () => {
-  const [name, setUsername] = useState(() => '');
-  const [pass, setPassword] = useState(() => '');
-  const [flag, setFlag] = useState(() => true);
-
-  const [response, setResponse] = useState(() => {});
+  let [name, setName] = useState(() => "");
+  let [pass, setPass] = useState(() => "");
+  let [flag, setFlag] = useState(() => false);
+  let [isLoading, setIsLoading] = useState(() => false);
+  let [isRedirect, setIsRedirect] = useState(() => false);
+  let initialRender = useRef(true);
 
   useEffect(() => {
-    if (!(name && pass)) {
+    if (initialRender.current) {
+      initialRender.current = false;
       return;
     }
-    console.log('Calling api started');
+
+    if (!(name && pass)) {
+      toast.warn("Name and password cannot be empty", {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+
+      return;
+    }
+
+    setIsLoading(true);
+
     axios
       .post(BASE_URL, {
         userName: name,
         password: pass,
       })
       .then((response) => {
-        setResponse(response.data);
         proceedResponse(response.data);
       })
       .catch((error) => {
-        if (error.response.status === 400) {
-          alert('You cannot register such a user');
-        } else if (error.request) {
-          console.log(error.request);
-        } else if (error.message) {
-          console.log(error.message);
+        console.warn(error.response);
+        if (typeof error.response.data !== "object") {
+          toast.error(`${error.response.data}`, {
+            position: toast.POSITION.BOTTOM_RIGHT,
+          });
+          return;
         }
+        toast.error("Internal server error", {
+          position: toast.POSITION.BOTTOM_RIGHT,
+        });
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
-  });
+  }, [flag]);
 
   const proceedResponse = (response) => {
     try {
-      alert(`Welcome ${response.userName}`);
-      localStorage.setItem('token', response.token);
+      toast.success(`Welcome ${response.userName}`, {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+      localStorage.setItem("token", response.token);
+      setIsRedirect(true);
     } catch (ex) {
-      console.log(ex);
-      alert('Impossible to register such a user');
+      console.warn(ex);
+      toast.error(`${ex.error}`, {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
     }
   };
 
   return (
     <>
+      {isRedirect ? <Navigate push to="/main" /> : null}
       <div className="registration">
         <div className="registration__body">
           <div className="registration__info">
@@ -70,11 +97,11 @@ const Register = () => {
 
             <form className="registration__form">
               <div className="registration__inputName input">
-                <label className="label">Full Name</label>
+                <label className="label">Username</label>
                 <input
                   type="text"
                   onChange={(e) => {
-                    setUsername(e.target.value);
+                    setName(e.target.value);
                   }}
                 />
               </div>
@@ -83,19 +110,24 @@ const Register = () => {
                 <input
                   type="password"
                   onChange={(e) => {
-                    setPassword(e.target.value);
+                    setPass(e.target.value);
                   }}
                 />
               </div>
-
+            </form>
+            {isLoading ? (
+              <LinearProgress />
+            ) : (
               <button
                 className="registration__btn"
                 type="submit"
-                onClick={() => setFlag(!flag)}
+                onClick={() => {
+                  setFlag(!flag);
+                }}
               >
                 Get Started
               </button>
-            </form>
+            )}
 
             <div className="registration__exit">
               <Link href="/main">Back to main page</Link>
