f5f0fa812f96b7af7ff23e79c9de70c86b18ecf1
Danil-Dovhopolyi <65538352+Danil-Dovhopolyi@users.noreply.github.com>
2022-04-25 15:51:02 +0300

    Implemented styles for form

--- OnlineShopUI/online-shop-ui/src/components/Register/Register.js
+++ OnlineShopUI/online-shop-ui/src/components/Register/Register.js
@@ -1,19 +1,20 @@
-import { Link, LinearProgress } from "@mui/material";
-import { React, useState, useEffect, useRef } from "react";
-import { toast } from "react-toastify";
-import { Navigate } from "react-router-dom";
-import "react-toastify/dist/ReactToastify.css";
-import axios from "axios";
-import "./Register.scss";
-import RegisterBG from "../../img/banner-bg.png";
+import { Link, LinearProgress } from '@mui/material';
+import { React, useState, useEffect, useRef } from 'react';
+import { toast } from 'react-toastify';
+import { Navigate } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
+import './Register.scss';
+import RegisterBG from '../../img/registr-bg.jpg';
+import Header from '../Header';
 
-const BASE_URL = "https://localhost:44330/api/user/register/";
+const BASE_URL = 'https://localhost:44330/api/user/register/';
 
 toast.configure();
 
 const Register = () => {
-  let [name, setName] = useState(() => "");
-  let [pass, setPass] = useState(() => "");
+  let [name, setName] = useState(() => '');
+  let [pass, setPass] = useState(() => '');
   let [flag, setFlag] = useState(() => false);
   let [isLoading, setIsLoading] = useState(() => false);
   let [isRedirect, setIsRedirect] = useState(() => false);
@@ -26,7 +27,7 @@ const Register = () => {
     }
 
     if (!(name && pass)) {
-      toast.warn("Name and password cannot be empty", {
+      toast.warn('Name and password cannot be empty', {
         position: toast.POSITION.BOTTOM_RIGHT,
       });
 
@@ -44,16 +45,19 @@ const Register = () => {
         proceedResponse(response.data);
       })
       .catch((error) => {
-        console.warn(error.response);
-        if (typeof error.response.data !== "object") {
-          toast.error(`${error.response.data}`, {
+        console.warn(error?.response);
+        try {
+          if (typeof error.response.data !== 'object') {
+            toast.error(`${error.response.data}`, {
+              position: toast.POSITION.BOTTOM_RIGHT,
+            });
+            return;
+          }
+        } catch {
+          toast.error('Something went wrong', {
             position: toast.POSITION.BOTTOM_RIGHT,
           });
-          return;
         }
-        toast.error("Internal server error", {
-          position: toast.POSITION.BOTTOM_RIGHT,
-        });
       })
       .finally(() => {
         setIsLoading(false);
@@ -65,7 +69,7 @@ const Register = () => {
       toast.success(`Welcome ${response.userName}`, {
         position: toast.POSITION.BOTTOM_RIGHT,
       });
-      localStorage.setItem("token", response.token);
+      localStorage.setItem('token', response.token);
       setIsRedirect(true);
     } catch (ex) {
       console.warn(ex);
@@ -79,6 +83,8 @@ const Register = () => {
     <>
       {isRedirect ? <Navigate push to="/main" /> : null}
       <div className="registration">
+        <Header />
+
         <div className="registration__body">
           <div className="registration__info">
             <div className="registration__header">
@@ -105,6 +111,7 @@ const Register = () => {
                   }}
                 />
               </div>
+
               <div className="registration__inputPassword input">
                 <label className="label">Password</label>
                 <input
@@ -133,9 +140,10 @@ const Register = () => {
               <Link href="/main">Back to main page</Link>
             </div>
           </div>
-          <div className="registration__img">
-            <img src={RegisterBG} alt="Bg" />
-          </div>
+          {/* <div className="registration__img">
+            <h1>Hello!</h1>
+            <span>We were waiting for you</span>
+          </div> */}
         </div>
       </div>
     </>
