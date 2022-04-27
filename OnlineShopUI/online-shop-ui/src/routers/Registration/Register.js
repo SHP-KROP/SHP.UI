842254ac6416a924efd072ee6d49af4ef8950aaa
ManeMag <akkofsasha@gmail.com>
2022-04-27 21:52:11 +0300

    Refactored register page

--- OnlineShopUI/online-shop-ui/src/routers/Registration/Register.js
+++ OnlineShopUI/online-shop-ui/src/routers/Registration/Register.js
@@ -1,82 +1,13 @@
-import { Link, LinearProgress } from '@mui/material';
-import { React, useState, useEffect, useRef } from 'react';
-import { toast } from 'react-toastify';
-import { Navigate } from 'react-router-dom';
-import 'react-toastify/dist/ReactToastify.css';
-import axios from 'axios';
-import './Register.scss';
-import Feedback from '../../components/FeedbackATop/Feedback';
-
-const BASE_URL = 'https://localhost:44330/api/user/register/';
-
-toast.configure();
+import { Link, LinearProgress } from "@mui/material";
+import { Navigate } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import "./Register.scss";
+import Feedback from "../../components/FeedbackATop/Feedback";
+import useRegister from "./Logic/RegisterLogic";
 
 const Register = () => {
-  let [name, setName] = useState(() => '');
-  let [pass, setPass] = useState(() => '');
-  let [flag, setFlag] = useState(() => false);
-  let [isLoading, setIsLoading] = useState(() => false);
-  let [isRedirect, setIsRedirect] = useState(() => false);
-  let initialRender = useRef(true);
-
-  useEffect(() => {
-    if (initialRender.current) {
-      initialRender.current = false;
-      return;
-    }
-
-    if (!(name && pass)) {
-      toast.warn('Name and password cannot be empty', {
-        position: toast.POSITION.BOTTOM_RIGHT,
-      });
-
-      return;
-    }
-
-    setIsLoading(true);
-
-    axios
-      .post(BASE_URL, {
-        userName: name,
-        password: pass,
-      })
-      .then((response) => {
-        proceedResponse(response.data);
-      })
-      .catch((error) => {
-        console.warn(error?.response);
-        try {
-          if (typeof error.response.data !== 'object') {
-            toast.error(`${error.response.data}`, {
-              position: toast.POSITION.BOTTOM_RIGHT,
-            });
-            return;
-          }
-        } catch {
-          toast.error('Something went wrong', {
-            position: toast.POSITION.BOTTOM_RIGHT,
-          });
-        }
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, [flag]);
-
-  const proceedResponse = (response) => {
-    try {
-      toast.success(`Welcome ${response.userName}`, {
-        position: toast.POSITION.BOTTOM_RIGHT,
-      });
-      localStorage.setItem('token', response.token);
-      setIsRedirect(true);
-    } catch (ex) {
-      console.warn(ex);
-      toast.error(`${ex.error}`, {
-        position: toast.POSITION.BOTTOM_RIGHT,
-      });
-    }
-  };
+  const [isRedirect, isLoading, setFlag, setName, setPass, flag] =
+    useRegister();
 
   return (
     <>
