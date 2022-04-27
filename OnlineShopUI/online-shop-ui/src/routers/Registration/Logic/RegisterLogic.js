8179b40f87678d458713a841efab3a62b7155493
ManeMag <akkofsasha@gmail.com>
2022-04-27 21:54:24 +0300

    Added register logic

---
+++ OnlineShopUI/online-shop-ui/src/routers/Registration/Logic/RegisterLogic.js
@@ -0,0 +1,80 @@
+import { useState, useEffect, useRef } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
+import "react-toastify/dist/ReactToastify.css";
+
+const BASE_URL = "https://localhost:44330/api/user/register/";
+
+toast.configure();
+
+const useRegister = () => {
+  let [name, setName] = useState(() => "");
+  let [pass, setPass] = useState(() => "");
+  let [flag, setFlag] = useState(() => false);
+  let [isLoading, setIsLoading] = useState(() => false);
+  let [isRedirect, setIsRedirect] = useState(() => false);
+  let initialRender = useRef(true);
+
+  useEffect(() => {
+    if (initialRender.current) {
+      initialRender.current = false;
+      return;
+    }
+
+    if (!(name && pass)) {
+      toast.warn("Name and password cannot be empty", {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+
+      return;
+    }
+
+    setIsLoading(true);
+
+    axios
+      .post(BASE_URL, {
+        userName: name,
+        password: pass,
+      })
+      .then((response) => {
+        proceedResponse(response.data);
+      })
+      .catch((error) => {
+        console.warn(error?.response);
+        try {
+          if (typeof error.response.data !== "object") {
+            toast.error(`${error.response.data}`, {
+              position: toast.POSITION.BOTTOM_RIGHT,
+            });
+            return;
+          }
+        } catch {
+          toast.error("Something went wrong", {
+            position: toast.POSITION.BOTTOM_RIGHT,
+          });
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [flag]);
+
+  const proceedResponse = (response) => {
+    try {
+      toast.success(`Welcome ${response.userName}`, {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+      localStorage.setItem("token", response.token);
+      setIsRedirect(true);
+    } catch (ex) {
+      console.warn(ex);
+      toast.error(`${ex.error}`, {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+    }
+  };
+
+  return [isRedirect, isLoading, setFlag, setName, setPass, flag];
+};
+
+export default useRegister;
