d15920703fb37622ea8480c8cb9ff2a57f3c0558
Danil-Dovhopolyi <65538352+Danil-Dovhopolyi@users.noreply.github.com>
2022-04-28 16:54:39 +0300

    Implented validation in the registration form

--- OnlineShopUI/online-shop-ui/src/routers/Registration/Register.js
+++ OnlineShopUI/online-shop-ui/src/routers/Registration/Register.js
@@ -4,10 +4,26 @@ import 'react-toastify/dist/ReactToastify.css';
 import './Register.scss';
 import Feedback from '../../components/FeedbackATop/Feedback';
 import useRegister from './Logic/RegisterLogic';
+import { useForm } from 'react-hook-form';
 
 const Register = () => {
   const [isRedirect, isLoading, setFlag, setName, setPass, flag] =
     useRegister();
+
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    reset,
+  } = useForm({
+    mode: 'onBlur',
+  });
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+    console.log(errors.Password);
+    reset();
+  };
 
   return (
     <>
@@ -31,7 +47,10 @@ const Register = () => {
             {/* Calling to the methods */}
             <div className="messages"></div>
 
-            <form className="registration__form">
+            <form
+              className="registration__form"
+              onSubmit={handleSubmit(onSubmit)}
+            >
               <div className="registration__inputName input">
                 <label className="label">Username</label>
                 <input
@@ -39,7 +58,19 @@ const Register = () => {
                   onChange={(e) => {
                     setName(e.target.value);
                   }}
+                  {...register('Username', {
+                    required: 'Field Username required for registration',
+                    minLength: {
+                      value: 3,
+                      message: 'Username must be more than 3 characters',
+                    },
+                  })}
                 />
+              </div>
+              <div className="registration__error">
+                {errors?.Username && (
+                  <p>{errors?.Username.message || 'Error!'}</p>
+                )}
               </div>
 
               <div className="registration__inputPassword input">
@@ -49,22 +80,38 @@ const Register = () => {
                   onChange={(e) => {
                     setPass(e.target.value);
                   }}
+                  {...register('Password', {
+                    required: 'Field Password required for registration',
+                    pattern: {
+                      value: /^[A-Za-z]+$/i,
+                      message: 'Password must included',
+                    },
+                    minLength: {
+                      value: 5,
+                      message: 'Password must be more than 5 characters',
+                    },
+                  })}
                 />
               </div>
+
+              <div className="registration__error">
+                {errors?.Password &&
+                  'Password must be longer than 5 characters and contain at least one special character'}
+              </div>
+              {isLoading ? (
+                <LinearProgress />
+              ) : (
+                <input
+                  type="submit"
+                  className="registration__btn"
+                  onClick={() => {
+                    setFlag(!flag);
+                  }}
+                  value="Get Started"
+                  disabled={!isValid}
+                />
+              )}
             </form>
-            {isLoading ? (
-              <LinearProgress />
-            ) : (
-              <button
-                className="registration__btn"
-                type="submit"
-                onClick={() => {
-                  setFlag(!flag);
-                }}
-              >
-                Get Started
-              </button>
-            )}
 
             <div className="registration__exit">
               <Link href="/home">Back to main page</Link>
