89358ff1d4398b71556e92840ebfb4be30c754d5
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2022-04-28 22:03:33 +0300

    Added configuration for cloudinary

---
+++ OnlineShopAPI/OnlineShopAPI/Options/ConfigurationOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineShopAPI.Options
+{
+    public static class ConfigurationOptions
+    {
+        public const string Root = "Cloudinary";
+
+        public const string ApiKey = Root + ":ApiKey";
+        public const string ApiSecret = Root + ":ApiSecret";
+    }
+}
