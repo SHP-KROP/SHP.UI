db2ec61132d50a8dd4e4d4ffa3835ca0d244e96c
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2022-05-04 16:30:10 +0300

    Added endpoint for getting liked products

---
+++ OnlineShopAPI/OnlineShopAPI/Controllers/LikeController.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using DAL.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShopAPI.Constants;
+using OnlineShopAPI.DTO.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopAPI.Controllers
+{
+    [Authorize(Roles = Roles.Buyer)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LikeController : ControllerBase
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
+
+        public LikeController(IUnitOfWork uow, IMapper mapper)
+        {
+            _uow = uow;
+            _mapper = mapper;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetLikedProductsByUser()
+        {
+            var products = await _uow?.UserRepository.GetProductsLikedByUser(this.GetUserId());
+
+            if (products is null || !products.Any())
+            {
+                return BadRequest("No liked products");
+            }
+
+            return Ok(_mapper.Map<IEnumerable<ProductDto>>(products));
+        }
+
+        // GET api/<LikeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<LikeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LikeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LikeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
