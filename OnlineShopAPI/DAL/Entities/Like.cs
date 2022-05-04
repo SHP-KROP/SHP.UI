fd48b943ae77ae1a61f220837a924d5c0dace4b1
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2022-05-04 15:44:18 +0300

    Added Like entity

---
+++ OnlineShopAPI/DAL/Entities/Like.cs
@@ -0,0 +1,13 @@
+﻿namespace DAL.Entities
+{
+    public class Like
+    {
+        public AppUser User { get; set; }
+
+        public Product Product { get; set; }
+
+        public int UserId { get; set; }
+
+        public int ProductId { get; set; }
+    }
+}
