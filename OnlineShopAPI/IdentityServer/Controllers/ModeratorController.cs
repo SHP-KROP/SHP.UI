d363d7ba02d5885f1c7bfae51b2145e04b8a0191
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2022-08-04 22:36:11 +0300

    @SHP-51: Added empty moderator controller

---
+++ OnlineShopAPI/IdentityServer/Controllers/ModeratorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModeratorController : ControllerBase
+    {
+
+    }
+}
