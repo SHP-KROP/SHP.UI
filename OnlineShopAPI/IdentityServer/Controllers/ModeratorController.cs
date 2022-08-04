eb6025e8f7a1e8520ccf7ef055201cc1af971103
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2022-08-04 22:39:41 +0300

    @SHP-51: Added Authorize attribute and roles

--- OnlineShopAPI/IdentityServer/Controllers/ModeratorController.cs
+++ OnlineShopAPI/IdentityServer/Controllers/ModeratorController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityServer.Controllers
 {
+    [Authorize(Roles = Roles.AdminOrModer)]
     [Route("api/[controller]")]
     [ApiController]
     public class ModeratorController : ControllerBase
