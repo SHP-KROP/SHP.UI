ff10b77cf1c9512dd4ca73362b8d3620a4bca0cf
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2022-08-04 22:58:48 +0300

    @SHP-51: Added GetAllUsers endpoint

--- OnlineShopAPI/IdentityServer/Controllers/ModeratorController.cs
+++ OnlineShopAPI/IdentityServer/Controllers/ModeratorController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AutoMapper;
+using DAL.Interfaces;
+using IdentityServer.DTO;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace IdentityServer.Controllers
@@ -9,15 +15,34 @@ namespace IdentityServer.Controllers
     [ApiController]
     public class ModeratorController : ControllerBase
     {
-        public ModeratorController()
-        {
+        private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
+        private readonly ILogger<ModeratorController> _logger;
 
+        public ModeratorController(IUnitOfWork uow, IMapper mapper, ILogger<ModeratorController> logger)
+        {
+            _uow = uow;
+            _mapper = mapper;
+            _logger = logger;
         }
 
         [HttpGet]
-        public async Task<ActionResult> GetUsers()
+        public async Task<ActionResult<IEnumerable<UserDto>>> GetAllUsers()
         {
-            return Ok();
+            _logger.LogInformation("GetAllUsers start executing");
+
+            var users = await _uow.UserRepository.GetUsersAsync();
+
+            if (users is null || !users.Any())
+            {
+                _logger.LogInformation("There were not any users. No content 204 returned");
+                return NoContent();
+            }
+
+            var userDtos = _mapper.Map<IEnumerable<UserDto>>(users);
+
+            _logger.LogInformation("Usere have been found and mapped. Returned Ok 200");
+            return Ok(userDtos);
         }
     }
 }
