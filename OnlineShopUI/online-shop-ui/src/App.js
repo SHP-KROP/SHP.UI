6d3f1214861b818e112403c6e9abac19c8f960d6
DanilDovhopolyi <danil.dovhopolyi@gmail.com>
2023-05-09 21:56:48 +0300

    Deleted several project for payment

--- OnlineShopUI/online-shop-ui/src/App.js
+++ OnlineShopUI/online-shop-ui/src/App.js
@@ -8,7 +8,6 @@ import CrudSeller from './routers/Seller/CRUD/CrudSeller';
 import ProductInfo from './routers/Product/ProductInfo';
 function App() {
   const userProvider = useAuthProvider();
-
   return (
     <>
       <UserContext.Provider value={userProvider}>
@@ -19,7 +18,6 @@ function App() {
           <Route path="/likes" element={<LikesPage />}></Route>
           <Route path="/seller" element={<CrudSeller />}></Route>
           <Route path="/product/:productName" element={<ProductInfo />}></Route>
-          <Route path="http://localhost:3001/"></Route>
         </Routes>
       </UserContext.Provider>
     </>
